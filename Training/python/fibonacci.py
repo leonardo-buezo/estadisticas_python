6935e96f469e3d133b698e208285cec7f11b1a8f
Leonardo Buezo <leonardo.buezo@appdirect.com>
2021-05-03 23:23:17 -0300

    probabilidades

---
+++ Training/python/fibonacci.py
@@ -0,0 +1,29 @@
+import sys, math
+from functools import lru_cache
+
+@lru_cache(maxsize = 1000)
+def fibonacci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1
+    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+
+def fibonacci_dinamico(n, memo = {}):
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except KeyError:
+        resultado = fibonacci_dinamico(n-1, memo) +  fibonacci_dinamico(n-2, memo)
+        memo[n] = resultado
+        return resultado
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10002)
+    n = int(input('Escoge un número: '))
+    result =  fibonacci_recursivo(n)
+    print(result)
+    x = min(5, 10, 25)
+    print(x)
+    
